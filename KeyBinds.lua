b55f2a01ed113a1b25ad0ce062a5830e5793683b
XNEOFF <111242581+XNEOFF@users.noreply.github.com>
2023-06-05 22:48:09 +0300

    KeyBinds.lua

--- KeyBinds.lua
+++ KeyBinds.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://scriptblox.com/raw/Universal-Script-Keyboard-uploaded-10996"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/KeyBinds/main/KeyBinds-Source.lua"))()
